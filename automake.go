2ba60394466c62211c7fe5fbe48fbc37b753d027
richardpct <noreply>
2018-11-14 21:31:56 +0100

    first commit

---
+++ automake.go
@@ -0,0 +1,89 @@
+// automake package
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/richardpct/pkgsrc"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+var destdir = flag.String("destdir", "", "directory installation")
+var pkg pkgsrc.Pkg
+
+const (
+	name     = "automake"
+	vers     = "1.16.1"
+	ext      = "tar.xz"
+	url      = "http://ftp.gnu.org/gnu/automake"
+	hashType = "sha256"
+	hash     = "5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921"
+)
+
+func checkArgs() error {
+	if *destdir == "" {
+		return fmt.Errorf("Argument destdir is missing")
+	}
+	return nil
+}
+
+func configure() {
+	fmt.Println("Waiting while configuring ...")
+	cmd := exec.Command("./configure",
+		"--prefix="+*destdir)
+	path := os.Getenv("PATH")
+	cmd.Env = append(os.Environ(), "PATH="+path+":"+*destdir+"/bin")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func build() {
+	fmt.Println("Waiting while compiling ...")
+	cmd := exec.Command("make")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func install() {
+	fmt.Println("Waiting while installing ...")
+	cmd := exec.Command("make", "install")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func main() {
+	flag.Parse()
+	if err := checkArgs(); err != nil {
+		log.Fatal(err)
+	}
+
+	pkg.Init(name, vers, ext, url, hashType, hash)
+	pkg.CleanWorkdir()
+	if !pkg.CheckSum() {
+		pkg.DownloadPkg()
+	}
+	if !pkg.CheckSum() {
+		log.Fatal("Package is corrupted")
+	}
+
+	pkg.Unpack()
+	wdPkgName := path.Join(pkgsrc.Workdir, pkg.PkgName)
+	if err := os.Chdir(wdPkgName); err != nil {
+		log.Fatal(err)
+	}
+	configure()
+	build()
+	install()
+}
